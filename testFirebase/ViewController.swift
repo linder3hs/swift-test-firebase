38730264bdd5dff169c9e9549a90fdc8b31d0b0f
linder3hs <linderhassinger00@gmail.com>
2021-06-10 11:37:51 -0500

    init commit :zap:

--- testFirebase/ViewController.swift
+++ testFirebase/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func onClickSingUp(_ sender: Any) {
+        let email = emailTxt.text!
+        let password = passwordTxt.text!
+        
+        Auth.auth().createUser(withEmail: email, password: password) { [self] error, info in
+            self.store(email: email, password: password)
+            performSegue(withIdentifier: "nextScreen", sender: nil)
+        }
+    
+    }
+    func store(email:String, password: String) {
+        let user = [
+            "email": email,
+            "name": password,
+        ]
+        
+        let id = Int.random(in: 1..<1000)
+        
+        Database.database().reference().child("users").child(String(id)).setValue(user)
+    }
 
 }
 
